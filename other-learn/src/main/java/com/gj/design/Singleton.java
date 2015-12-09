7b45c830bb71de745259d5ccb3f19bd47ff37a85
awayy1432 <awayy1432@163.com>
2015-12-09 18:39:02 +0800

    单例

---
+++ other-learn/src/main/java/com/gj/design/Singleton.java
@@ -0,0 +1,12 @@
+package com.gj.design;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guojing
+ * Date: 15-12-9
+ * Time: 下午6:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Singleton {
+
+}

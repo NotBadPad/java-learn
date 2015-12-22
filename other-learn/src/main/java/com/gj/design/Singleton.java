fde0b7542ecb1607329b411c2978e6dcaf6bca5c
awayy1432 <awayy1432@163.com>
2015-12-22 15:04:22 +0800

    Integer test

--- other-learn/src/main/java/com/gj/design/Singleton.java
+++ other-learn/src/main/java/com/gj/design/Singleton.java
@@ -9,4 +9,5 @@ package com.gj.design;
  */
 public class Singleton {
 
+
 }
---
+++ other-learn/src/test/java/com/gj/core/StringTest.java
@@ -0,0 +1,21 @@
+package com.gj.core;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guojing
+ * Date: 15-12-22
+ * Time: 下午2:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringTest {
+
+    @Test
+    public void testIntegerHashCode(){
+        Integer i1 = new Integer(1);
+        Integer i2 = new Integer(1);
+        System.out.println(i1.hashCode());
+        System.out.println(i2.hashCode());
+    }
+}
